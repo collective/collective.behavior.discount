c8d44052cfc5912ba124cc1d0fae2a7b78c71aad
Taito Horiuchi <taito.horiuchi@gmail.com>
2015-06-25 15:11:06 +0300

    release 0.3.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='collective.behavior.discount',
-    version='0.3',
+    version='0.3.1',
     description="Behavior of discount related fields.",
     long_description=long_description,
     classifiers=[
